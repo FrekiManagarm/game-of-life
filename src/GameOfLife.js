0c81f507053e288e42e8d1d5f097900c82f22d61
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-06-26 18:47:57 +0200

    more fixes

---
+++ src/GameOfLife.js
@@ -0,0 +1,199 @@
+import React from 'react';
+import './Game.css';
+
+
+const TAILLE_CELLULE = 20;
+const LARGEUR = 1400;
+const HAUTEUR = 800;
+
+
+class Cellule extends React.Component {
+
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div className="Cell" style={{
+                left: `${TAILLE_CELLULE * x + 1}px`,
+                top: `${TAILLE_CELLULE * y + 1}px`,
+                width: `${TAILLE_CELLULE - 1}px`,
+                height: `${TAILLE_CELLULE - 1}px`,
+            }} />
+        );
+    }
+}
+
+
+class Game extends React.Component {
+
+    constructor() {
+        super();
+        this.rows = HAUTEUR / TAILLE_CELLULE;
+        this.cols = LARGEUR / TAILLE_CELLULE;
+
+        this.board = this.makeEmptyBoard();
+    }
+
+    state = {
+        cells: [],
+        isRunning: false,
+        interval: 100,
+    }
+
+    makeEmptyBoard() {
+        let board = [];
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                board[y][x] = false;
+            }
+        }
+
+        return board;
+    }
+
+    getElementOffset() {
+        const rect = this.boardRef.getBoundingClientRect();
+        const doc = document.documentElement;
+
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) - doc.clientTop,
+        };
+    }
+
+    makeCells() {
+        let cells = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (this.board[y][x]) {
+                    cells.push({ x, y });
+                }
+            }
+        }
+
+        return cells;
+    }
+
+    handleClick = (event) => {
+
+        const elemOffset = this.getElementOffset();
+        const offsetX = event.clientX - elemOffset.x;
+        const offsetY = event.clientY - elemOffset.y;
+        
+        const x = Math.floor(offsetX / TAILLE_CELLULE);
+        const y = Math.floor(offsetY / TAILLE_CELLULE);
+
+        if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+            this.board[y][x] = !this.board[y][x];
+        }
+
+        this.setState({ cells: this.makeCells() });
+    }
+
+    runGame = () => {
+        this.setState({ isRunning: true });
+        this.runIteration();
+    }
+
+    stopGame = () => {
+        this.setState({ isRunning: false });
+        if (this.timeoutHandler) {
+            window.clearTimeout(this.timeoutHandler);
+            this.timeoutHandler = null;
+        }
+    }
+
+    runIteration() {
+        let newBoard = this.makeEmptyBoard();
+
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let neighbors = this.compteurVoisins(this.board, x, y);
+                if (this.board[y][x]) {
+                    if (neighbors === 2 || neighbors === 3) {
+                        newBoard[y][x] = true;
+                    } else {
+                        newBoard[y][x] = false;
+                    }
+                } else {
+                    if (!this.board[y][x] && neighbors === 3) {
+                        newBoard[y][x] = true;
+                    }
+                }
+            }
+        }
+
+        this.board = newBoard;
+        this.setState({ cells: this.makeCells() });
+
+        this.timeoutHandler = window.setTimeout(() => {
+            this.runIteration();
+        }, this.state.interval);
+    }
+
+    
+    compteurVoisins(board, x, y) {
+        let neighbors = 0;
+        const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+        for (let i = 0; i < dirs.length; i++) {
+            const dir = dirs[i];
+            let y1 = y + dir[0];
+            let x1 = x + dir[1];
+
+            if (x1 >= 0 && x1 < this.cols && y1 >= 0 && y1 < this.rows && board[y1][x1]) {
+                neighbors++;
+            }
+        }
+
+        return neighbors;
+    }
+
+    handleIntervalChange = (event) => {
+        this.setState({ interval: event.target.value });
+    }
+
+    handleClear = () => {
+        this.board = this.makeEmptyBoard();
+        this.setState({ cells: this.makeCells() });
+    }
+
+    dispoAleatoire = () => {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                this.board[y][x] = (Math.random() >= 0.5);
+            }
+        }
+
+        this.setState({ cells: this.makeCells() });
+    }
+
+    render() {
+        const { cells, isRunning } = this.state;
+        return (
+            <div>
+                <div className="Board"
+                    style={{ width: LARGEUR, height: HAUTEUR, backgroundSize: `${TAILLE_CELLULE}px ${TAILLE_CELLULE}px`}}
+                    onClick={this.handleClick}
+                    ref={(n) => { this.boardRef = n; }}>
+
+                    {cells.map(cell => (
+                        <Cellule x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+                    ))}
+                </div>
+
+                <div className="controls">
+                    Intervale des mise à jour <input value={this.state.interval} onChange={this.handleIntervalChange} /> msec
+                    {isRunning ?
+                        <button className="button" onClick={this.stopGame}>Stop</button> :
+                        <button className="button" onClick={this.runGame}>Run</button>
+                    }
+                    <button className="button" onClick={this.dispoAleatoire}>Random</button>
+                    <button className="button" onClick={this.handleClear}>Clear</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Game;
